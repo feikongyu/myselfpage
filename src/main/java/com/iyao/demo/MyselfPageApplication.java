9f3ec91ed41ef25fa3b7d2f9d9e2d824c87804d6
diyueli <1227961108@qq.com>
2020-07-01 20:22:22 +0800

    新建项目

---
+++ src/main/java/com/iyao/demo/MyselfPageApplication.java
@@ -0,0 +1,13 @@
+package com.iyao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselfPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyselfPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iyao/demo/MyselfPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iyao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselfPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
